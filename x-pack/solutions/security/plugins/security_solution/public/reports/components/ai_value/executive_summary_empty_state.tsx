ecaf50e5a640a567cd6a72dd17d2a16fb3c1b48b
Valentina Glagoleva <valentina.glagoleva@elastic.co>
2026-04-16 12:25:23 +0200

    Exploring EUI layout

--- x-pack/solutions/security/plugins/security_solution/public/reports/components/ai_value/executive_summary_empty_state.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/reports/components/ai_value/executive_summary_empty_state.tsx
@@ -6,7 +6,55 @@
  */
 
 import React from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiText,
+  EuiSpacer,
+  EuiTitle,
+  EuiIcon,
+  EuiButtonEmpty,
+  EuiButtonIcon,
+} from '@elastic/eui';
+import analyticsSpeedAcceleration from './analytics-speed-accelation.svg';
 
 export const ExecutiveSummaryEmptyState: React.FC<{}> = () => {
-  return <div>{'Placeholder for the new design'}</div>;
+  return (
+    <>
+      <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
+        <EuiFlexItem grow={false} alignItems="center">
+          <EuiSpacer size="m" />
+          <EuiIcon
+            type={analyticsSpeedAcceleration}
+            size="original"
+            title="Analytics Speed Acceleration"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={6}>
+          <EuiSpacer size="s" />
+          <EuiTitle size="xs">
+            <p>{'No attacks detected yet'}</p>
+          </EuiTitle>
+          <EuiSpacer size="s" />
+          <EuiText size="s">
+            <p>{'Get started with Attack Discovery'}</p>
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty
+            size="s"
+            iconType="external"
+            iconSide="right"
+            color="primary"
+            onClick={() => {}}
+          >
+            {'Attack discovery'}
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonIcon iconType="cross" aria-label="Attack discovery" color="text" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </>
+  );
 };
